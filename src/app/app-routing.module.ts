458c67d033ded5c613170f7c81ce6701f2bb6c59
mihir <mihirgadwalkar@gmail.com>
2021-06-03 18:19:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
+import { PhotosComponent } from './photos/photos.component'
+import { ContactusComponent } from './contactus/contactus.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home',component:HomeComponent},
+  { path:'about',component:AboutComponent},
+  { path:'photos',component:PhotosComponent},
+  { path:'contactus',component:ContactusComponent},
+  { path:'', redirectTo:'/photos',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContactusComponent,
+    HomeComponent,
+    AboutComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
