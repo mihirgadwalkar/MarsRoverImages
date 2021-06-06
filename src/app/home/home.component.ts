e8d0d348aff9b43adc5d38f83c0a70e0a1574ad8
mihir <mihirgadwalkar@gmail.com>
2021-06-06 15:41:11 +0530

    functioning with simple design completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routeData:Router) { }
 
   ngOnInit(): void {
   }
 
+  getFullData(){
+    this.routeData.navigateByUrl('rovers')
+  }
 
 
 }
